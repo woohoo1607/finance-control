c560e6915ef64e91a66e5a42b97b76f237d6d9b0
woohoo1607 <45455797+woohoo1607@users.noreply.github.com>
2019-09-17 23:37:04 +0300

    Updates

---
+++ precache-manifest.96ea9a32934edae7a32ed565aafaba0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1bd167e06fe990445c90c264601c2a",
+    "url": "/finance-control/index.html"
+  },
+  {
+    "revision": "04215082ccc2c2619af1",
+    "url": "/finance-control/static/css/main.a49ad6a7.chunk.css"
+  },
+  {
+    "revision": "f23b7a06a193b756f9c3",
+    "url": "/finance-control/static/js/2.0dfb495d.chunk.js"
+  },
+  {
+    "revision": "04215082ccc2c2619af1",
+    "url": "/finance-control/static/js/main.cc3b2f75.chunk.js"
+  },
+  {
+    "revision": "95388d0ef4141dd77fc4",
+    "url": "/finance-control/static/js/runtime~main.1e3ef59e.js"
+  },
+  {
+    "revision": "266c44f4d5e4b96af11fed54448b3ed1",
+    "url": "/finance-control/static/media/background.266c44f4.jpg"
+  },
+  {
+    "revision": "972ddd4d653d929f5c4bebc4217f252b",
+    "url": "/finance-control/static/media/logo.972ddd4d.jpg"
+  }
+]);
