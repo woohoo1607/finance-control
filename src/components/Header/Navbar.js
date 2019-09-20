b06722abf72ff571f3d8243256a672cd8fef8c77
woohoo1607 <45455797+woohoo1607@users.noreply.github.com>
2019-09-20 22:01:26 +0300

    final release

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {NavLink} from "react-router-dom"; 
-import styles from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
